7fa9be00799325435027377625c88f7bb758e7af
pankaj-design <74971560+pankaj-design@users.noreply.github.com>
2023-03-25 15:35:10 +0530

    removing map

--- src/Map.js
+++ src/Map.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Map = () => {
   return (
     <>
-    <div>map</div>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30689174.43435246!2d64.37810984725671!3d20.124153082294264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1676999089158!5m2!1sen!2sin" width="100%" height="300px" style={{border:0}} allowFullScreen="" loading="lazy" title='itsmap' referrerPolicy="no-referrer-when-downgrade"></iframe>
+    {/* <div>map</div> */}
+{/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30689174.43435246!2d64.37810984725671!3d20.124153082294264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1676999089158!5m2!1sen!2sin" width="100%" height="300px" style={{border:0}} allowFullScreen="" loading="lazy" title='itsmap' referrerPolicy="no-referrer-when-downgrade"></iframe> */}
 
  <div className='P3 row'>
 <div className='B2 col-sm -4'>
   <ul>
-    <li><h3>More to Learn</h3></li>
+    <li><h3>Top searches</h3></li>
     <li>Top searches</li>
     <li>Places to visit</li>
     <li>About</li>
